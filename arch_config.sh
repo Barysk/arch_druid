aed80745c0fe86e75978f0f219c3c89a5c917528
Boris Komarov <boris.komarov@proton.me>
2025-07-24 20:05:34 +0200

    v0.1.0

--- arch_config.sh
+++ arch_config.sh
@@ -6,7 +6,151 @@
 
 TMP_FOLDER="arch_config"
 RST_PATH="$HOME/$TMP_FOLDER"
+
 GPU_VENDOR=""
+
+INTEL_PACKAGES=""
+
+AMD_PACKAGES="
+	vulkan-radeon
+	xf86-video-amdgpu
+"
+
+# you probably don't need it
+# amdvlk
+
+NVIDIA_PACKAGES="
+	nvidia-open-dkms
+	nvidia-utils
+	lib32-nvidia-utils
+"
+
+
+PACMAN_PACKAGES="
+	base
+	base-devel
+	neovim
+	xorg
+	wayland
+	uwsm
+	hyprland
+	man-db
+	xdg-desktop-portal-gtk
+	xdg-desktop-portal-hyprland
+	udisks2
+	tree-sitter-cli
+	tree
+	tmux
+	tldr
+	timidity++
+	freepats-general-midi
+	gtk3
+	gtk4
+	thermald
+	texlive
+	tailscale
+	swww
+	steam
+	shotcut
+	ripgrep
+	redshift
+	reaper
+	raylib
+	odin
+	qutebrowser
+	qt6ct
+	qt5ct
+	qt5ct-wayland
+	qjackctl
+	qbittorrent
+	pipewire
+	alsa-utils
+	lib32-pipewire
+	pipewire-alsa
+	lib32-alsa-plugins
+	pipewire-jack
+	lib32-pipewire-jack
+	pipewire-pulse
+	playerctl
+	prismlauncher
+	sof-firmware
+	wireplumber
+	lib32-mesa
+	wl-clipboard
+	wine-staging
+	wine-mono
+	xf86-video-vesa
+	pcmanfm-gtk3
+	pavucontrol
+	papirus-icon-theme
+	obs-studio
+	telegram-desktop
+	nwg-look
+	networkmanager
+	network-manager-applet
+	mpv
+	materia-gtk-theme
+	mako
+	lutris
+	linux-zen-headers
+	kvantum
+	krita
+	kitty
+	jp2a
+	imv
+	imagemagick
+	hyprsunset
+	hyprshot
+	hyprpolkitagent
+	hyprlock
+	htop
+	btop
+	gst-plugin-pipewire
+	github-cli
+	fzf
+	fastfetch
+	electron
+	dust
+	clang
+	docker
+	carla
+	brightnessctl
+	bluez
+	bluez-utils
+	biber
+	bat
+	audacity
+"
+
+PARU_PACKAGES="
+	anki-bin
+	aseprite
+	auto-cpufreq
+	postbird-bin
+	blockbench-bin
+	bluetuith-bin
+	brave-bin
+	librewolf-bin
+	proton-ge-custom-bin
+	tofi
+	vesktop-bin
+	xpadneo-dkms
+	pcsx2-latest-bin
+	rpcs3-bin
+"
+
+# Probably unneded paru packages
+# betterbird-bin
+# electron6-bin
+# giblib
+# httpdirfs
+# mpvpaper
+# neothesia
+# pixelorama-bin
+# ttf-mplus-git
+# ucl
+# vscodium-bin
+# xnp2
 
 
 echo "GPU?"
@@ -57,10 +201,52 @@ echo "downloading paru-bin"
 git clone https://aur.archlinux.org/paru-bin.git
 
 
-echo "installing paru-bin"
-cd paru-bin
-makepkg -sri
+echo "downloading dotfiles"
+git clone https://github.com/barysk/dot_hyprland
+git clone https://github.com/barysk/nvim
 
 
-cd $RST_PATH
+# echo "installing paru-bin"
+# cd paru-bin
+# makepkg -sri
+# cd $RST_PATH
+#
+#
+# echo "applying dotfiles"
+# # nvim
+# cd nvim
+# rm -rf .git
+# cd ..
+# mv nvim $HOME/.config/
+# # hypr # TODO compile modules
+# cd dot_hyprland
+# rm -rf .git
+# cd ..
+# mv dot_hyprland/dot_config/* $HOME/.config/
+# mv dot_hyprland/dot_fonts $HOME/.fonts
+# mv dot_hyprland/dot_bashrc $HOME/.bashrc
+
+
+# echo "downloading packages using pacman"
+#
+
+
+# echo "downloading packages using paru"
+# paru -S ${PARU_PACKAGES[@]}
+
+# echo "setting default uwsm"
+# uwsm blah
+
+# echo "removing $TMP_FOLDER at $HOME"
+# cd $HOME
+# rm -rf $TMP_FOLDER
+
+# echo "updating tldr"
+# tldr --update
+
+# echo "setting up auto-cpufreq"
+# sudo auto-cpufreq install
+
+echo "All done!"
+echo "You can remove folder $TMP_FOLDER at $HOME"
 
